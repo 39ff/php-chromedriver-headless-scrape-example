6d0b8f620177a1043447327934f129659139da7d
39ff <discover@cse.ne.jp>
2020-03-04 04:32:26 +0900

    added google search example

--- src/example.php
+++ src/example.php
@@ -13,7 +13,7 @@ try {
     $options = new ChromeOptions();
     $options->addArguments([
         '--headless',
-        '--window-size=700,700',
+        '--window-size=1920,1080',
         '--ignore-certificate-errors',
         '--disable-popup-blocking',
         '--disable-web-security',
@@ -36,17 +36,35 @@ try {
     $driver = RemoteWebDriver::create($host, $caps);
     $driver->get('http://google.co.jp');
 
+    $driver->wait(10, 500)->until(
+        WebDriverExpectedCondition::visibilityOfElementLocated(WebDriverBy::cssSelector("form input[type=text]"))
+    );
+
+    $driver
+        ->findElement(WebDriverBy::name('q'))
+        ->sendKeys('ブラックハットSEO　方法')
+        ->submit();
+
     //フッター要素が出るまで待機する
     $driver->wait(10, 500)->until(
         WebDriverExpectedCondition::elementToBeClickable(WebDriverBy::id("foot"))
     );
-
+    $searchResults = $driver->findElements(WebDriverBy::cssSelector("#search .srg .g"));
+    foreach ($searchResults as $remoteWebElement){
+        $href = $remoteWebElement->findElement(WebDriverBy::cssSelector('.r a'))->getAttribute("href");
+        var_dump($href);
+    }
     //ページのHTMLソース
     $source = $driver->getPageSource();
-    var_dump($source);
+    file_put_contents("google_search_result.html",$source);
+    $driver->takeScreenshot('google_search.png');
 
-    $driver->wait(5);
-    $driver->close();
+    //最初の検索結果1件をクリック
+    $driver->findElement(WebDriverBy::cssSelector("#search .srg .g a"))->click();
+    $driver->wait(10, 500)->until(
+        WebDriverExpectedCondition::visibilityOfElementLocated(WebDriverBy::tagName("body"))
+    );
+    $driver->takeScreenshot('google_search_first_page.png');
 
 }catch (\Facebook\WebDriver\Exception\WebDriverException $e){
     var_dump($e);
